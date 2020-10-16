f6470f3ed88913dc040934569c9236433cf277c6
Zfour <499984532@qq.com>
2020-10-17 00:07:41 +0800

    Site updated: 2020-10-17 00:06:40

--- js/artitalkkey.js
+++ js/artitalkkey.js
@@ -5,9 +5,11 @@ new Artitalk({
 	color1: "#b30070",
 	color2: "rgb(229, 91, 178)",
 });
-$(".power").children("br").remove();
+//$(".power").children("br").remove();
 $(".power").css("padding","10px");
 $(".power").css("text-align","left");
 $("#switchUser").css("padding-right","20px");
 $("#pubShuo").css("padding-right","20px");
 $("#pubComment").css("padding-right","20px");
+
+
