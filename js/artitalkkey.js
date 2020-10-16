e548ec9b401a56cb93d9e9639a6b3229eb55139e
Zfour <499984532@qq.com>
2020-10-16 22:11:20 +0800

    Site updated: 2020-10-16 22:10:22

---
+++ js/artitalkkey.js
@@ -0,0 +1,13 @@
+new Artitalk({
+    appId: '14vXx1qgCU3UarkOgkXfTQnd-MdYXbMMI',
+    appKey: 'h4qlLGHlHsyHCWOxT1JqpBV3',
+	pageSize: 1,
+	color1: "#b30070",
+	color2: "rgb(229, 91, 178)",
+});
+$(".power").children("br").remove();
+$(".power").css("padding","10px");
+$(".power").css("text-align","left");
+$("#switchUser").css("padding-right","20px");
+$("#pubShuo").css("padding-right","20px");
+$("#pubComment").css("padding-right","20px");
