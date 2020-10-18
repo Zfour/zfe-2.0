199d7434a28f1648314b635fab301812c12c3be3
Zfour <499984532@qq.com>
2020-10-18 21:07:27 +0800

    Site updated: 2020-10-18 21:06:37

---
+++ js/shuo.js
@@ -0,0 +1,18 @@
+function shuovisualchange(){
+  let obt=document.getElementById("shuovisualchange");
+  let odiv= $(".power").css("display");
+
+  
+    if(odiv == "none"){
+       $(".power").css({"display":"inline"});
+      obt.innerHTML="隐藏";
+    }
+    else{
+     $(".power").css({"display":"none"});
+      obt.innerHTML="编辑";
+    }
+ 
+}
+window.onload =function() { 
+$(".power").css({"display":"none"});
+};
